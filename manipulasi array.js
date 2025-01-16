4f7e6a64c9b37312b4b2a2472f969f9bdec7926a
MaajidSigma <maajid.pratama@gmail.com>
2025-01-16 11:41:43 +0700

    Add files via upload

---
+++ manipulasi array.js
@@ -0,0 +1,7 @@
+let angka = [1, 2, 3];
+
+angka.push(4);
+console.log(angka);
+
+angka.unshift(0);
+console.log(angka);
---
+++ manipulasi string.js
@@ -0,0 +1,5 @@
+const teks = "maajid";
+const hurufKecil = teks.toLowerCase();
+const hurufBesar = teks.toUpperCase();
+console.log(hurufKecil);
+console.log(hurufBesar);
